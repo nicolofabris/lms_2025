622539c41cea40a93bc677cab3b23d0ff1ef713b
Nicolo Fabris <nicolo.fabriss@gmail.com>
2025-02-14 11:57:22 +0100

    sign in

--- client/src/components/Student/Navbar.jsx
+++ client/src/components/Student/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
+import { useClerk, UserButton, useUser } from "@clerk/clerk-react";
 
 const Navbar = () => {
   const isCourseListPage = location.pathname.includes("/course-list");
+
+  const { openSignIn } = useClerk();
+  const { user } = useUser();
 
   return (
     <div
@@ -17,15 +21,43 @@ const Navbar = () => {
         className="w-28 lg:w-32 cursor-pointer"
       />
       <div className="hidden md:flex items-center gap-5 text-gray-500">
-        <div>
-          <button>Become Educator</button>|{" "}
-          <Link to="/my-enrollments">My Enrollments</Link>
+        <div className="flex items-center gap-5">
+          {user && (
+            <>
+              <button>Become Educator</button>|{" "}
+              <Link to="/my-enrollments">My Enrollments</Link>
+            </>
+          )}
         </div>
-        <button className="bg-blue-600 text-white px-5 py-2 rounded-full">
-          Create Account
-        </button>
+        {user ? (
+          <UserButton />
+        ) : (
+          <button
+            className="bg-blue-600 text-white px-5 py-2 rounded-full"
+            onClick={() => openSignIn()}
+          >
+            Create Account
+          </button>
+        )}
       </div>
-      <div></div>
+
+      <div className="md:hidden flex items-center gap-2 sm:gap-5 text-gray-500">
+        <div className="flex items-center gap-1 sm:gap-2 max-sm:ext-xst">
+          {user && (
+            <>
+              <button>Become Educator</button>|{" "}
+              <Link to="/my-enrollments">My Enrollments</Link>
+            </>
+          )}
+        </div>
+        {user ? (
+          <UserButton />
+        ) : (
+          <button onClick={() => openSignIn()}>
+            <img src={assets.user_icon} alt="user-icon" />
+          </button>
+        )}
+      </div>
     </div>
   );
 };
