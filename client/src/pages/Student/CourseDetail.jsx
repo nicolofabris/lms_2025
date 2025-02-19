79442b0118e757821c834ff8e54b081ecf435ab3
Nicolo Fabris <nicolo.fabriss@gmail.com>
2025-02-19 11:13:28 +0100

    course detail page

--- client/src/pages/Student/CourseDetail.jsx
+++ client/src/pages/Student/CourseDetail.jsx
@@ -1,7 +1,31 @@
-const CourseDetail = () => {
-  return (
-	<div>CourseDetail</div>
-  )
-}
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { AppContext } from "../../context/AddContext";
 
-export default CourseDetail
+const CourseDetail = () => {
+  const { id } = useParams();
+
+  const [courseData, setCourseData] = useState(null);
+
+  const { allCourses } = useContext(AppContext);
+
+  const fetchCourseData = async () => {
+    const findCourse = allCourses.find((course) => course._id === id);
+    setCourseData(findCourse);
+  };
+
+  useEffect(() => {
+    fetchCourseData();
+  }, []);
+
+  return (
+    <div className="flex md:flex-row flex-col-reverse gap-10 relative items-start justify-between md:px-36 px-8 md:pt-30 pt-20 text-left">
+      <div className="absolute top-0 left-0 w-full h-section-height -z-1 bg-gradient-to-b from-cyan-100/70"></div>
+      <div></div>
+
+      <div></div>
+    </div>
+  );
+};
+
+export default CourseDetail;
