07e28335ae6122c6e051aa490d5967b630a380c0
Nicolo Fabris <nicolo.fabriss@gmail.com>
2025-02-20 10:35:22 +0100

    course detail page

--- client/src/components/Student/CourseCard.jsx
+++ client/src/components/Student/CourseCard.jsx
@@ -14,8 +14,8 @@ const CourseCard = ({ course }) => {
     >
       <img src={course.courseThumbnail} alt="thumbnail" className="w-full" />
       <div className="p-3 text-left">
-        <h3 className="ext-base font-semibold">{course.courseTitle}</h3>
-        <p className="text-gray-500">{course.educator.name}</p>
+        <h3 className="text-base font-semibold">{course.courseTitle}</h3>
+        <p className="text-gray-500">GreatStack</p>
         <div className="flex items-center space-x-2">
           <p>{calculateRating(course)}</p>
           <div className="flex">
