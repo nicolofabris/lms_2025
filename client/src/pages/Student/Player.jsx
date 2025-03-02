8cf9010656c33076555116fd646364875a159ee9
Nicolo Fabris <nicolo.fabriss@gmail.com>
2025-03-02 12:44:25 +0100

    player page

--- client/src/pages/Student/Player.jsx
+++ client/src/pages/Student/Player.jsx
@@ -1,110 +1,142 @@
-import {useParams} from "react-router-dom";
-import {AppContext} from "../../context/AddContext";
-import {useContext, useEffect, useState} from "react";
-import {assets} from "../../assets/assets";
+import { useParams } from "react-router-dom";
+import { AppContext } from "../../context/AddContext";
+import { useContext, useEffect, useState } from "react";
+import { assets } from "../../assets/assets";
 import humanizeDuration from "humanize-duration";
+import YouTube from "react-youtube";
+import Footer from "../../components/Student/Footer";
 
 const Player = () => {
-	const {enrolledCourses, calculateChapterTime} = useContext(AppContext);
-	const {courseId} = useParams();
-	const [courseData, setCourseData] = useState(null);
-	const [openSection, setOpenSection] = useState({});
-	const [playerData, setPlayerData] = useState(null);
+  const { enrolledCourses, calculateChapterTime } = useContext(AppContext);
+  const { courseId } = useParams();
+  const [courseData, setCourseData] = useState(null);
+  const [openSection, setOpenSection] = useState({});
+  const [playerData, setPlayerData] = useState(null);
 
-	const getCourseData = () => {
-		enrolledCourses.map((course) => {
-			if (course._id === courseId) {
-				setCourseData(course)
-			}
-		})
-	}
-	const toggleSection = (index) => {
-		setOpenSection((prev) => ({ ...prev, [index]: !prev[index] }));
-	  };
+  const getCourseData = () => {
+    enrolledCourses.map((course) => {
+      if (course._id === courseId) {
+        setCourseData(course);
+      }
+    });
+  };
+  const toggleSection = (index) => {
+    setOpenSection((prev) => ({ ...prev, [index]: !prev[index] }));
+  };
 
-	useEffect(() => {
-		getCourseData()
-	}, [enrolledCourses])
+  useEffect(() => {
+    getCourseData();
+  }, [enrolledCourses]);
   return (
     <>
       <div className="p-4 sm:p-10 flex flex-col-reverse md:grid md:grid-cols-2 gap-10 md:px-36">
         <div className="text-gray-800">
           <h2 className="text-xl font-semibold">Course Structure</h2>
           <div className="pt-5">
-            { courseData && courseData.courseContent.map((chapter, index) => (
-              <div
-                className="border border-gray-300 bg-white mb-2 rounded"
-                key={index}
-              >
+            {courseData &&
+              courseData.courseContent.map((chapter, index) => (
                 <div
-                  onClick={() => toggleSection(index)}
-                  className="flex items-center justify-between py-3 px-4 cursor-pointer select-none"
+                  className="border border-gray-300 bg-white mb-2 rounded"
+                  key={index}
                 >
-                  <div className="flex items-center gap-2">
-                    <img
-                      src={assets.down_arrow_icon}
-                      alt="arrow"
-                      className={`transform transition-transform ${
-                        openSection[index] ? "rotate-180" : ""
-                      }`}
-                    />
-                    <p className="font-medium md:text-base text-sm">
-                      {chapter.chapterTitle}
+                  <div
+                    onClick={() => toggleSection(index)}
+                    className="flex items-center justify-between py-3 px-4 cursor-pointer select-none"
+                  >
+                    <div className="flex items-center gap-2">
+                      <img
+                        src={assets.down_arrow_icon}
+                        alt="arrow"
+                        className={`transform transition-transform ${
+                          openSection[index] ? "rotate-180" : ""
+                        }`}
+                      />
+                      <p className="font-medium md:text-base text-sm">
+                        {chapter.chapterTitle}
+                      </p>
+                    </div>
+                    <p className="text-sm md:text-default">
+                      {chapter.chapterContent.length} lectures -{" "}
+                      {calculateChapterTime(chapter)}
                     </p>
                   </div>
-                  <p className="text-sm md:text-default">
-                    {chapter.chapterContent.length} lectures -{" "}
-                    {calculateChapterTime(chapter)}
-                  </p>
-                </div>
 
-                <div
-                  className={`overflow-hidden transition-all duration-300 ${
-                    openSection[index] ? "max-h-96" : "max-h-0"
-                  }`}
-                >
-                  <ul className="list-disc md:pl-10 pl-4 pr-4 py-2 text-gray-600 border-t border-gray-300">
-                    {chapter.chapterContent.map((lecture, index) => (
-                      <li className="flex items gap-2 py-1-start" key={index}>
-                        <img
-                          src={false ? assets.blue_tick_icon : assets.play_icon}
-                          alt="play"
-                          className="w-4 h-4 mt-1"
-                        />
-                        <div className="flex items-center justify-between w-full text-gray-800 text-xs md:text-default">
-                          <p>{lecture.lectureTitle}</p>
-                          <div className="flex gap-2">
-                            {lecture.lectureUrl && (
-                              <p
-                                onClick={() =>
-                                  setPlayerData({
-									...lecture, chapter: index + 1, lecture: index + 1
-								  })
-                                }
-                                className="text-blue-500 cursor-pointer"
-                              >
-                                Watch
-                              </p>
-                            )}
-                            <p>
-                              {humanizeDuration(
-                                lecture.lectureDuration * 60 * 1000,
-                                { units: ["h", "m"] }
+                  <div
+                    className={`overflow-hidden transition-all duration-300 ${
+                      openSection[index] ? "max-h-96" : "max-h-0"
+                    }`}
+                  >
+                    <ul className="list-disc md:pl-10 pl-4 pr-4 py-2 text-gray-600 border-t border-gray-300">
+                      {chapter.chapterContent.map((lecture, index) => (
+                        <li className="flex items gap-2 py-1-start" key={index}>
+                          <img
+                            src={
+                              false ? assets.blue_tick_icon : assets.play_icon
+                            }
+                            alt="play"
+                            className="w-4 h-4 mt-1"
+                          />
+                          <div className="flex items-center justify-between w-full text-gray-800 text-xs md:text-default">
+                            <p>{lecture.lectureTitle}</p>
+                            <div className="flex gap-2">
+                              {lecture.lectureUrl && (
+                                <p
+                                  onClick={() =>
+                                    setPlayerData({
+                                      ...lecture,
+                                      chapter: index + 1,
+                                      lecture: index + 1,
+                                    })
+                                  }
+                                  className="text-blue-500 cursor-pointer"
+                                >
+                                  Watch
+                                </p>
                               )}
-                            </p>
+                              <p>
+                                {humanizeDuration(
+                                  lecture.lectureDuration * 60 * 1000,
+                                  { units: ["h", "m"] }
+                                )}
+                              </p>
+                            </div>
                           </div>
-                        </div>
-                      </li>
-                    ))}
-                  </ul>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+          </div>
+
+          <div className="flex items-center gap-2 py-3 mt-10">
+            <h1 className="text-xl font-bold">Rate this course:</h1>
           </div>
         </div>
 
-        <div></div>
+        <div className="md:mt-10">
+          {playerData ? (
+            <div>
+              <YouTube
+                videoId={playerData.lectureUrl.split("/").pop()}
+                iframeClassName="w-full aspect-video"
+              />
+              <div className="flex justify-between items-center mt-1">
+                <p>
+                  {playerData.chapter}.{playerData.lecture}{" "}
+                  {playerData.lectureTitle}
+                </p>
+                <button className="text-blue-600">
+                  {false ? "Completed" : "Mark as complete"}
+                </button>
+              </div>
+            </div>
+          ) : courseData && courseData.courseThumbnail ? (
+            <img src={courseData.courseThumbnail} alt="Course Thumbnail" />
+          ) : null}
+        </div>
       </div>
+      <Footer />
     </>
   );
 };
