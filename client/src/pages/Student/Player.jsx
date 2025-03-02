3f9b1af3b4279a02ccc192f6c1cc860a66f564bf
Nicolo Fabris <nicolo.fabriss@gmail.com>
2025-03-02 12:51:24 +0100

    rating component added

--- client/src/pages/Student/Player.jsx
+++ client/src/pages/Student/Player.jsx
@@ -5,6 +5,7 @@ import { assets } from "../../assets/assets";
 import humanizeDuration from "humanize-duration";
 import YouTube from "react-youtube";
 import Footer from "../../components/Student/Footer";
+import Rating from "../../components/Student/Rating";
 
 const Player = () => {
   const { enrolledCourses, calculateChapterTime } = useContext(AppContext);
@@ -111,6 +112,7 @@ const Player = () => {
 
           <div className="flex items-center gap-2 py-3 mt-10">
             <h1 className="text-xl font-bold">Rate this course:</h1>
+			<Rating initialRating={0}/>
           </div>
         </div>
 
