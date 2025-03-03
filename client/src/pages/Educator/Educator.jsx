f5dcb8b77a5f7e793a12476c8d4a3a73ae333d07
Nicolo Fabris <nicolo.fabriss@gmail.com>
2025-03-03 11:22:29 +0100

    educator sidebar

--- client/src/pages/Educator/Educator.jsx
+++ client/src/pages/Educator/Educator.jsx
@@ -1,13 +1,19 @@
-import { Outlet } from "react-router-dom"
-import Navbar from "../../components/Educator/Navbar"
+import { Outlet } from "react-router-dom";
+import Navbar from "../../components/Educator/Navbar";
+import Sidebar from "../../components/Educator/Sidebar";
 
 const Educator = () => {
   return (
-	<div className="text-default min-h-screen bg-white">
-		<Navbar/>
-		<div><Outlet/></div>
-	</div>
-  )
-}
+    <div className="text-default min-h-screen bg-white">
+      <Navbar />
+      <div className="flex">
+        <Sidebar />
+        <div className="flex-1">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Educator
+export default Educator;
